125e630c6f865d983cd44d109042ae425d60dfb4
Louis <xunmeibuyue@outlook.com>
2022-05-23 19:23:09 +0800

    fix a bug of cv2.findContours output

--- code/idac/blobdet/otsu_blobdetector.py
+++ code/idac/blobdet/otsu_blobdetector.py
@@ -34,7 +34,7 @@ class OtsuBlobDetector(Blobdetector):
         startingid = startingid
         # Init counters for counting number of images
         count = 0
-        _, contours, _= cv2.findContours(255-binary.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours, _= cv2.findContours(255-binary.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         for cnt in contours:
             x, y, w, h = cv2.boundingRect(cnt)
             crop_img = original[y:y + h, x:x + w]
